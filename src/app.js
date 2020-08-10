9e46da465a8498babedbf4217f8345848dc41e31
balajibalajanc <balajibalajanci@gmail.com>
2020-08-10 22:56:05 +0530

    Done Dynamic front end & static front end

--- src/app.js
+++ src/app.js
@@ -2,14 +2,35 @@ const path = require('path')
 const express= require('express');
 
 const public_dir_path=path.join(__dirname,'../public');
-const help_dir_path=path.join(__dirname,'../public/help.html');
-const about_dir_path=path.join(__dirname,'../public/about.html');
+const view_dir_path=path.join(__dirname,'../Templates');
+
+// const help_dir_path=path.join(__dirname,'../public/help');
+// const about_dir_path=path.join(__dirname,'../public/about.html');
 
 const app=express()
 
+app.set('view engine','hbs');
+app.set('views',view_dir_path);
+
+
 app.use(express.static(public_dir_path))
 
+app.get('',(req,res)=>
+{
+    res.render('index',{
+        title:'Hi successfully done Dyamic implementation'
+    });
+})
 
+app.get('/help',(req,res)=>
+{
+res.render('help',{name :'FAQ'});
+})
+
+app.get('/about',(req,res)=>
+{
+res.render('about',{title :'FAQ',desc:' balaji &  co'});
+})
 
 // app.get('/',(req,res)=>{
 

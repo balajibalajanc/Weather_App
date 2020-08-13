82f521ac26277137cbebf924d21da0449d5df479
balajibalajanc <balajibalajanci@gmail.com>
2020-08-13 19:59:30 +0530

    Build a full web application

--- public/js/app.js
+++ public/js/app.js
@@ -2,22 +2,26 @@ console.log('Client side java script loaded successfully!');
 
 const weatherform=document.querySelector('form');
 const searchform=document.querySelector('input');
+const message_1=document.querySelector('#msg_1');
+const message_2=document.querySelector('#msg_2');
 
+message_1.textContent="Output";
 
 weatherform.addEventListener('submit',(e)=>{
     e.preventDefault();
     const location=searchform.value
 
-    console.log(location);
-
     fetch('http://localhost:3000/weather?search='+encodeURIComponent(location)).then((response)=>{
     response.json().then((data)=>{
         if(data.error)
         {
-            console.log(data.error);
+           message_2.textContent= data.error;
         }   
-        else{console.log(data);}
-    })
+        else{
+            message_2.textContent='Location you searched for :'+ data.location_details+' Temperature :'+data.Forecast;
+
+        }
+    })      
 })
 })
 
